e3bbb24aae80aa3b19427287026a3e81a5561f86
khg1 <khg1@duck.com>
2026-04-11 14:15:39 -0400

    reorganized

---
+++ LUT/sigmoid.py
@@ -0,0 +1,16 @@
+import numpy as np
+import os
+
+i = np.arange(256, dtype=np.uint8)
+x = i.view(np.int8).astype(np.float64)
+
+sigmoid = 1.0 / (1.0 + np.exp(-x))
+
+lut = np.clip(np.round(sigmoid*256), -32768, 32767).astype(np.int16)
+unsigned_lut = lut.view(np.uint16)
+
+os.makedirs("lut", exist_ok=True)
+
+with open("./lut/sigmoid_q88.hex", "w") as f:
+    for value in unsigned_lut:
+        f.write(f"{value & 0xFFFF:04x}\n")
