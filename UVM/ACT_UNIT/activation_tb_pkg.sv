7eee2b71b1c56169b4780d8753931f175bdd56dd
khg9056 <khg9056@ecs02.poly.edu>
2026-04-13 14:13:04 -0400

    added uvm package and tb_top

---
+++ UVM/ACT_UNIT/activation_tb_pkg.sv
@@ -0,0 +1,14 @@
+package activation_tb_pkg;
+	import uvm_pkg::*;
+	`include "uvm_macros.svh"
+
+	`include "activation_seq_item.sv"
+   	`include "activation_sequence.sv"
+   	`include "activation_driver.sv"
+   	`include "activation_monitor.sv"
+   	`include "activation_agent.sv"
+   	`include "activation_scoreboard.sv"
+   	`include "activation_subscriber.sv"
+   	`include "activation_env.sv"
+   	`include "activation_test.sv"
+endpackage
--- UVM/ACT_UNIT/tb_top.sv
+++ UVM/ACT_UNIT/tb_top.sv
@@ -1,2 +1,54 @@
 module tb_top;
+
+	import uvm_pkg::*;
+	import activation_tb_pkg::*;
+
+	localparam int NUM_LANES = 16;
+	localparam int ACC_WIDTH = 16;
+
+	logic	clk;
+	logic	resetn;
+
+	initial begin
+		clk = 0;
+		forever #5 clk = ~clk;
+	end
+
+	initial begin
+		resetn = 0;
+		#20 resetn = 1;
+	end
+
+	activation_if #(NUM_LANES, ACC_WIDTH) vif(clk, resetn);
+
+	activation_unit #(.NUM_LANES(NUM_LANES), .ACC_WIDTH(ACC_WIDTH)) dut(
+		.clk(clk),
+		.resetn(resetn),
+		.en(vif.en),
+		.fn_sel(vif.fn_sel),
+		.data_in(vif.data_in),
+		.data_out(vif.data_out),
+		.data_valid(vif.data_valid),
+		.ready(vif.ready)
+	);
+
+	bind activation_unit activation_sva #(NUM_LANES, ACC_WIDTH) sva_inst (
+		.clk(clk),
+		.resetn(resetn),
+		.en(en),
+		.fn_sel(fn_sel),
+		.data_valid(data_valid),
+		.ready(ready)
+	);
+
+	initial begin
+		uvm_config_db#(virtual activation_if#(NUM_LANES, ACC_WIDTH))::set(null, "*", "vif", vif);
+		run_test("activation_test");
+	end
+
+	initial begin
+		$dumpfile("dump.vcd");
+		$dumpvars(0, tb_top);
+	end
+
 endmodule
