31be752771ab3fb96869a15ebaa6bc173d8dec64
khg1 <ikhg@duck.com>
2026-07-17 16:36:13 -0400

    updated the path

--- uvm/ACT_UNIT/activation_tb_pkg.sv
+++ uvm/ACT_UNIT/activation_tb_pkg.sv
@@ -2,13 +2,13 @@ package activation_tb_pkg;
 	import uvm_pkg::*;
 	`include "uvm_macros.svh"
 
-	`include "../UVM/ACT_UNIT/activation_seq_item.sv"
-   	`include "../UVM/ACT_UNIT/activation_sequence.sv"
-   	`include "../UVM/ACT_UNIT/activation_driver.sv"
-   	`include "../UVM/ACT_UNIT/activation_monitor.sv"
-   	`include "../UVM/ACT_UNIT/activation_agent.sv"
-   	`include "../UVM/ACT_UNIT/activation_scoreboard.sv"
-   	`include "../UVM/ACT_UNIT/activation_coverage.sv"
-   	`include "../UVM/ACT_UNIT/activation_env.sv"
-   	`include "../UVM/ACT_UNIT/activation_test.sv"
+	`include "../uvm/ACT_UNIT/activation_seq_item.sv"
+   	`include "../uvm/ACT_UNIT/activation_sequence.sv"
+   	`include "../uvm/ACT_UNIT/activation_driver.sv"
+   	`include "../uvm/ACT_UNIT/activation_monitor.sv"
+   	`include "../uvm/ACT_UNIT/activation_agent.sv"
+   	`include "../uvm/ACT_UNIT/activation_scoreboard.sv"
+   	`include "../uvm/ACT_UNIT/activation_coverage.sv"
+   	`include "../uvm/ACT_UNIT/activation_env.sv"
+   	`include "../uvm/ACT_UNIT/activation_test.sv"
 endpackage
